31e03d67890846c2b60ff8e82194b006232bc7fe
abhinavp403 <abhinavp403@gmail.com>
2025-05-06 20:29:05 -0400

    Set up AndroidApplicationComposeConventionPlugin configuration

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -19,5 +19,9 @@ gradlePlugin {
             id = "runtracker.android.application"
             implementationClass = "AndroidApplicationConventionPlugin"
         }
+        register("androidApplicationCompose") {
+            id = "runtracker.android.application.compose"
+            implementationClass = "AndroidApplicationComposeConventionPlugin"
+        }
     }
 }
