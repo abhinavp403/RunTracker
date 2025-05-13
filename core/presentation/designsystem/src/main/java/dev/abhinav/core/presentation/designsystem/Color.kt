9bd7eed2ba1ada10f3eebaaf62f3ad896224f36c
abhinavp403 <abhinavp403@gmail.com>
2025-05-13 18:55:03 -0400

    Setup material theme

---
+++ core/presentation/designsystem/src/main/java/dev/abhinav/core/presentation/designsystem/Color.kt
@@ -0,0 +1,15 @@
+package dev.abhinav.core.presentation.designsystem
+
+import androidx.compose.ui.graphics.Color
+
+val RuntrackerGreen = Color(0xFF00F15E)
+val RuntrackerGreen30 = Color(0x4D00F15E)
+val RuntrackerGreen10 = Color(0x1A00F15E)
+val RuntrackerGreen5 = Color(0x0D00F15E)
+val RuntrackerBlack = Color(0xFF080707)
+val RuntrackerGray = Color(0xFF87938C)
+val RuntrackerGray40 = Color(0x6687938C)
+val RuntrackerDarkGray = Color(0xFF232624)
+val RuntrackerWhite = Color(0xFFFAFAFA)
+val RuntrackerDarkRed = Color(0xFFBB3D3D)
+val RuntrackerDarkRed5 = Color(0x0DBB3D3D)
