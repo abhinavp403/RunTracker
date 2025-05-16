726ed04121c5426a985d32162b3e44d9028b4f42
abhinavp403 <abhinavp403@gmail.com>
2025-05-15 21:26:37 -0400

    Register screen

---
+++ auth/presentation/src/main/java/dev/abhinav/auth/presentation/register/RegisterAction.kt
@@ -0,0 +1,7 @@
+package dev.abhinav.auth.presentation.register
+
+sealed interface RegisterAction {
+    data object OnTogglePasswordVisibilityClick: RegisterAction
+    data object OnLoginClick: RegisterAction
+    data object OnRegisterClick: RegisterAction
+}
---
+++ auth/presentation/src/main/java/dev/abhinav/auth/presentation/register/RegisterState.kt
@@ -0,0 +1,15 @@
+package dev.abhinav.auth.presentation.register
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.text2.input.TextFieldState
+import dev.abhinav.auth.domain.PasswordValidationState
+
+data class RegisterState @OptIn(ExperimentalFoundationApi::class) constructor(
+    val email: TextFieldState = TextFieldState(),
+    val isEmailValid: Boolean = false,
+    val password: TextFieldState = TextFieldState(),
+    val isPasswordVisible: Boolean = false,
+    val passwordValidationState: PasswordValidationState = PasswordValidationState(),
+    val isRegistering: Boolean = false,
+    val canRegister: Boolean = passwordValidationState.isValidPassword && !isRegistering
+)
