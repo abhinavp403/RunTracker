266e4bcc8fe94292ead0899f6ac88be917835ae1
abhinavp403 <abhinavp403@gmail.com>
2025-05-17 19:24:18 -0400

    Set up nav graph

--- app/src/main/java/dev/abhinav/runtracker/MainActivity.kt
+++ app/src/main/java/dev/abhinav/runtracker/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 import dev.abhinav.core.presentation.designsystem.RuntrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    val navController = rememberNavController()
+                    NavigationRoot(navController = navController)
                 }
             }
         }
