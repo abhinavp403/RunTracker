772db89555ca5b8a44e89cbb53fd68798a3833dc
abhinavp403 <abhinavp403@gmail.com>
2025-06-15 18:37:45 -0400

    Take snapshot of map after finishing run

--- run/presentation/src/main/java/dev/abhinav/run/presentation/active_run/ActiveRunViewModel.kt
+++ run/presentation/src/main/java/dev/abhinav/run/presentation/active_run/ActiveRunViewModel.kt
@@ -6,6 +6,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dev.abhinav.core.domain.location.Location
+import dev.abhinav.core.domain.run.Run
+import dev.abhinav.run.domain.LocationDataCalculator
 import dev.abhinav.run.domain.RunningTracker
 import dev.abhinav.run.presentation.active_run.service.ActiveRunService
 import kotlinx.coroutines.channels.Channel
@@ -16,6 +19,8 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 class ActiveRunViewModel(
     private val runningTracker: RunningTracker
@@ -82,7 +87,10 @@ class ActiveRunViewModel(
     fun onAction(action: ActiveRunAction) {
         when (action) {
             ActiveRunAction.OnFinishRunClick -> {
-                TODO()
+                state = state.copy(
+                    isRunFinished = true,
+                    isSavingRun = true
+                )
             }
             ActiveRunAction.OnResumeRunClick -> {
                 state = state.copy(shouldTrack = true)
@@ -113,6 +121,9 @@ class ActiveRunViewModel(
                     showLocationRationale = false
                 )
             }
+            is ActiveRunAction.OnRunProcessed -> {
+                finishRun(action.mapPictureBytes)
+            }
             else -> Unit
         }
     }
@@ -121,6 +132,33 @@ class ActiveRunViewModel(
         super.onCleared()
         if(!ActiveRunService.isServiceActive) {
             runningTracker.stopObservingLocation()
+        }
+    }
+
+    private fun finishRun(mapPictureBytes: ByteArray) {
+        val locations = state.runData.locations
+        if(locations.isEmpty() || locations.first().size <= 1) {
+            state = state.copy(isSavingRun = false)
+            return
+        }
+
+        viewModelScope.run {
+            val run = Run(
+                id = null,
+                duration = state.elapsedTime,
+                dateTimeUtc = ZonedDateTime.now()
+                    .withZoneSameInstant(ZoneId.of("UTC")),
+                distanceMeters = state.runData.distanceMeters,
+                location = state.currentLocation ?: Location(0.0, 0.0),
+                maxSpeedKmh = LocationDataCalculator.getMaxSpeedKmh(locations),
+                totalElevationMeters = LocationDataCalculator.getTotalElevationMeters(locations),
+                mapPictureUrl = null
+            )
+
+            // Save run in repository
+
+            runningTracker.finishRun()
+            state = state.copy(isSavingRun = false)
         }
     }
 }
